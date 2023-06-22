cde32dc35d2224aae1219b3da0a32ab5be9f6280
Heat Koemnak <heatkimnak@gmail.com>
2023-06-23 00:32:15 +0700

    added melek

---
+++ melek/statistic.py
@@ -0,0 +1,24 @@
+
+
+def Myfuction(x,y):
+
+
+    for i in range(x):
+        if(i>=10){
+            print(x,"*",y,"=",x*y)
+        }
+
+        
+Myfuction(1,1)
+Myfuction(2,1)
+Myfuction(3,1)
+Myfuction(4,1)
+Myfuction(6,1)
+Myfuction(7,1)
+Myfuction(8,1)
+Myfuction(9,1)
+Myfuction(10,1)
+
+
+
+
