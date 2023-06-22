fcaf0405071e77f280bcbf3c36aa233b8aab1470
Heat Koemnak <heatkimnak@gmail.com>
2023-06-23 00:16:53 +0700

    patrick version2

---
+++ patrick-version2/Josh_code.py
@@ -0,0 +1,32 @@
+while True:
+
+
+
+  #reciving the data from user
+    P=float(input("Enter Amount: "))#Amount's investment
+    i=float(input("Enter Interest rate: ")) #interest rate
+    n=int(input("Enter Year: "))#each years
+
+  
+    print("\n{:<10} {:<15} {:<20}".format("years", "Interest rate", "Return's Amount ",))
+    print("_"*40)#print line
+    
+
+    for x in range(n):
+
+        years = x+1 
+        x += 1
+        Final_value=P
+        Final_value = Final_value*(1+(i/100))**x
+        print("{:2}".format(years),
+          "{:14.2f}".format(i),
+          "{:18.2f}".format(Final_value))
+        print("_"*40)
+    check = input("Do you want to quit or start gain, enter Y to restart or another to end ?:")
+    if check.upper() == "Y": #loop back to the start
+
+
+        continue
+    print("Bye..!Have a nice day")
+
+    break #exit the program
