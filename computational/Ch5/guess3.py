a67bc7aa186e13c0b82df9a35a7e4492e5a70f65
Heat Koemnak <heatkimnak@gmail.com>
2023-06-23 00:03:53 +0700

    added computational

---
+++ computational/Ch5/guess3.py
@@ -0,0 +1,11 @@
+import random as rand
+compnumber = rand.randint(1, 3)
+i = 5
+for number in range(5):
+    usernumber = int(input("Choose a number between 1 and 100. You have " + str(i) + " guesses left. "))
+    if compnumber == usernumber:
+        print("You win!")
+    else:
+        i = i - 1
+
+print("You're out of guesses! You lose! ")
